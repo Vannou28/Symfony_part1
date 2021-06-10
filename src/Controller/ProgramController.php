a1ff0125b02980206f82f3689d481f88927ab72b
aurelien <vannier.autrelien2@gmail.com>
2021-06-10 10:11:54 +0200

    validate

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -118,6 +118,7 @@ class ProgramController extends AbstractController
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
         $form->handleRequest($request);
+        
         if ($form->isSubmitted() && $form->isValid()){
 
             $slugify = new Slugify($program->getTitle());
